62582a79ef5af6a767daa9352225dd5ba740c8da
FAYnim <yuana6898@gmail.com>
2026-02-25 04:15:26 +0700

    fix: data dir

--- includes/config.php
+++ includes/config.php
@@ -7,8 +7,8 @@ $pdo = $db->getConnection();
 define('SITE_URL', 'http://localhost/faydev/control-panel');
 define('UPLOAD_PATH', __DIR__ . '/uploads/');
 define('UPLOAD_URL', SITE_URL . '/uploads/');
-define('BACKUP_PATH', __DIR__ . '/backup/');
-define('JSON_PATH', __DIR__ . '/data/');
+define('BACKUP_PATH', dirname(__DIR__) . '/backup/');
+define('JSON_PATH', dirname(__DIR__) . '/data/');
 
 function getDB() {
     global $pdo;
